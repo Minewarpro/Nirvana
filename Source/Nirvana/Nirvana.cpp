a4f36967c47aec64379f3c47437873b4e62847b6
Minewarpro <hugo.roche13@hotmail.com>
2023-11-14 15:26:08 +0100

    InitCommit

---
+++ Source/Nirvana/Nirvana.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Nirvana.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Nirvana, "Nirvana" );
